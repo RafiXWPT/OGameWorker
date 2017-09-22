07eb3cba6e15b61a6bed01625522b4c5b6f0f6ba
Rafał Palej <palej@student.agh.edu.pl>
2017-09-22 22:14:19 +0200

    Add resources, HtmlAgaility and references

---
+++ Worker.Objects/Resources/Metal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker.Objects.Resources
+{
+    class Metal
+    {
+        public double Amount { get; set; } 
+    }
+}
